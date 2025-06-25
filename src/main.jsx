77c78d0af285f9a792051e43151dc196f42c6e28
Noman Arif <nomanarifk@gmail.com>
2025-06-25 10:36:29 +0500

    Open Form

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import DeadlineScreen from './components/DeadlineScreen'
-
+import App from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DeadlineScreen />
+    <App />
   </StrictMode>,
 )
